04f4ddd2c628e04b148e1df07f2755827851908d
Seb Pedley <seb@propeller-studios.co.uk>
2020-07-14 14:52:55 +0100

    Data service (Accounts) is now provided at the very top level in app.module.ts and accessible by all app areas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './accounts.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [AccountsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
